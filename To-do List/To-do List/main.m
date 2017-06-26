47ba3113569aa886564978f2a121611a0cf63e38
kevin00223 <2934193605@qq.com>
2017-06-26 17:49:23 +0800

    创建项目

---
+++ To-do List/To-do List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  To-do List
+//
+//  Created by likai on 2017/6/26.
+//  Copyright © 2017年 yinbake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
