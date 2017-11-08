c4604494073e8d98fc5ecd382678466ea639c8fc
kevin00223 <likai.new@gmail.com>
2017-11-08 16:34:00 +0800

    初始界面搭建

---
+++ MyLocations/MyLocations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by likai on 08/11/2017.
+//  Copyright © 2017 yinbake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
